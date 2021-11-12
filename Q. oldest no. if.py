3b39b37bd75589c8c95bd293490550b0c52d16c6
Sakshi Gangwar <94167310+sakshigangwar@users.noreply.github.com>
2021-11-12 14:52:59 +0530

    Add files via upload

---
+++ Q. oldest no. if.py
@@ -0,0 +1,9 @@
+# num1=int(input("enter the sister age"))
+# num2=int(input("enter the brother age"))
+# num3=int(input("enter the mother age"))
+# num4=int(input("enter the father age"))
+# total= (num1>20 or num2>30 or num3>45 and num4<50)
+# if num4<50:
+#     print( num4<50)
+# else:
+#     print("nothing")    
